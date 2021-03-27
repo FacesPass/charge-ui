29cc7140d2fa3923e24b66429c2b109f0bd2dc56
JiquanWang99 <951873902@qq.com>
2021-03-27 22:00:37 +0800

    test: 测试用例修改

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, RenderResult, wait, waitFor } from '@testing-library/react'
 import Menu, { IMenuProps } from './menu'
 import MenuItem from './menuItem'
+import SubMenu from './menuItem'
 
 
 const testProps: IMenuProps = {
   defaultIndex: '1',
   onSelect: jest.fn(),
-  className: 'test'
+  className: 'test',
 }
 
 const testVerticalProps: IMenuProps = {
@@ -21,15 +22,39 @@ const generateMenu = (props: IMenuProps) => {
       <MenuItem >normal</MenuItem>
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
+      <SubMenu>
+        <MenuItem>drop1</MenuItem>
+        <MenuItem>drop2</MenuItem>
+        <MenuItem>drop3</MenuItem>
+      </SubMenu>
     </Menu>
   )
 }
+
+const createStyleFile = () => {
+  const cssFile: string = `
+  .submenu {
+    display: none;
+  }
+  .submenu.open-submenu {
+    display: block;
+  }
+  `
+  const style = document.createElement('style')
+  style.type = 'text/css'
+  style.innerHTML = cssFile
+
+  return style
+}
+
+
 
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
 describe('测试Menu和MenuItem组件', () => {
   //通用函数，在每个测试用例开始前都会触发
   beforeEach(() => {
     wrapper = render(generateMenu(testProps))
+    wrapper.container.append(createStyleFile())
     menuElement = wrapper.getByTestId('test-menu')
     activeElement = wrapper.getByText('active')
     disabledElement = wrapper.getByText('disabled')
@@ -37,7 +62,7 @@ describe('测试Menu和MenuItem组件', () => {
   it('是否正确渲染使用默认参数时的Menu和MenuItem组件', () => {
     expect(menuElement).toBeInTheDocument()
     expect(menuElement).toHaveClass('menu test')
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
     expect(activeElement).toHaveClass('menu-item is-active')
     expect(disabledElement).toHaveClass('is-disabled')
   })
@@ -47,10 +72,10 @@ describe('测试Menu和MenuItem组件', () => {
     fireEvent.click(firstItem)
     expect(firstItem).toHaveClass('is-active')
     expect(activeElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).toHaveBeenCalledWith(0)   //回调函数是以0被调用
+    expect(testProps.onSelect).toHaveBeenCalledWith('0')   //回调函数是以0被调用
     fireEvent.click(disabledElement)
     expect(disabledElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(2)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('2')
   })
 
   it('正确渲染vertical垂直展示模式', () => {
@@ -61,4 +86,13 @@ describe('测试Menu和MenuItem组件', () => {
     expect(menuElement).toHaveClass('menu-vertical')
   })
 
+  // it('Submenu的渲染逻辑测试', async () => {
+  //   expect(wrapper.queryByText('drop1')).not.toBeVisible()
+  //   await waitFor(() => {
+
+  //   })
+
+  //   fireEvent.click(wrapper.getByText('drop1'))
+  //   expect(testProps.onSelect).toHaveBeenNthCalledWith('')
+  // })
 })
